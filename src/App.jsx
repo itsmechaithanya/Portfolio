61330ec4f74991eb09d1b27af4708c67ae0dbaa7
Chaithanya <itsmechaithanyam@gmail.com>
2026-01-30 12:52:07 -0600

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Home from './Components /Home'
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <div>
       <Home />
--- src/Components /Home.jsx
+++ src/Components /Home.jsx
@@ -21,17 +21,21 @@ function Home() {
             <h1 className='text-[3vh]'>Scroll Down</h1>
           </div>
         </div>
-        <div className='h-screen w-full absolute flex bg-[#EFF0FF] rounded-t-[10vh]'>
-          <div className='w-[50vw] h-full flex items-center justify-center relative'>
-            <div><img className=' absolute h-[15vh] left-[4vw]' src={Clip} alt="" /></div>
-            <div className='w-[35vw] h-[80vh] mt-[17vh] bg-green-200 rounded-[5vh] overflow-hidden'>
-              {/* <img src={Me} alt="" /> */}
-            </div>
+         <div className='bg-[#EFF0FF] h-auto w-full'>
+            <div 
+            // data-scroll data-scroll-speed="0.3"
+            className='h-screen w-full absolute flex bg-[#EFF0FF] rounded-t-[5vh]'>
+              <div className='w-[50vw] h-full flex items-center justify-center relative'>
+                <div><img className=' absolute h-[15vh] left-[4vw]' src={Clip} alt="" /></div>
+                <div className='w-[35vw] h-[80vh] mt-[17vh] bg-green-200 rounded-[5vh] overflow-hidden'>
+                  <img className='w-full h-full object-cover' src={Me} alt="" />
+                </div>
+              </div>
+              <div className='w-[50vw] h-full flex items-center justify-center pr-[10vw] pl-[5vw]'>
+                <h1 className='font-normal text-[3.5vh] text-[#8B8B8B]' >I am a <span className='text-black font-medium'>UI/UX Designer</span>  and <span className='text-black font-medium'>Developer</span> passionate about creating seamless, user-centered experiences. I have worked with early-stage startups, building and executing design strategies from scratch. Along with intuitive design and interactive web development, I am also involved in machine learning and computer vision. My focus is on elevating digital platforms through innovation and creativity.</h1>
+              </div>
           </div>
-          <div className='w-[50vw] h-full flex items-center justify-center pr-[10vw] pl-[5vw]'>
-            <h1 className='font-normal text-[3.5vh] text-[#8B8B8B]' >I am a <span>UI/UX designer</span>  and <span>developer</span> passionate about creating seamless, user-centered experiences. I have worked with early-stage startups, building and executing design strategies from scratch. Along with intuitive design and interactive web development, I am also involved in machine learning and computer vision. My focus is on elevating digital platforms through innovation and creativity.</h1>
-          </div>
-        </div>
+         </div>
         </div>
       </div>
     </div>
