3a7f2d96792a620fbe867f8ea108b34b3c6b26a1
Chaithanya <itsmechaithanyam@gmail.com>
2026-02-07 16:14:56 -0600

    feb7

--- src/Components /Navbar.jsx
+++ src/Components /Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div className='flex justify-center '>
-    <nav className='flex justify-between px-[2vw] mt-[1.5vh] py-[1vh] fixed z-30 backdrop-blur-lg w-[80vw] rounded-[5vh] bg-[#42424262] text-[white]'>
+    <nav className='flex justify-between px-[2vw] mt-[1.5vh] py-[1vh] fixed z-30 backdrop-blur-lg w-[80vw] rounded-[5vh] bg-[#0000006e] text-[white]'>
       <div>
         <Link to="/"> <h1 className='text-[4vh] font-bold '>Chay :)</h1></Link>
       </div>
